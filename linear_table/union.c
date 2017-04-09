cffa9ee44faf59da03f05d8e60ce2d555f352222
Qiyuan Pu <pqy7172@gmail.com>
2017-04-09 21:07:37 +0800

    add some notes

--- linear_table/union.c
+++ linear_table/union.c
@@ -15,6 +15,8 @@ typedef int ElemType;
 
 int Union(SqList *La, SqList Lb)
 {//insert all elements to linear table La that don't exist in linear table Lb
+
+  //get the length of the linear table
   int La_len = ListLength(*La), Lb_len = ListLength(Lb), i;
   ElemType *e;
 
@@ -22,10 +24,10 @@ int Union(SqList *La, SqList Lb)
 
   for (i = 1; i <= Lb_len; i++)
     {
-      GetElem(Lb, i, e);
+      GetElem(Lb, i, e); //get the i'th element to e
 
       if (!LocateElem(*La, *e, compare))
-	ListInsert(La, ++La_len, *e);
+	ListInsert(La, ++La_len, *e); // if e does not exist in La, so insert it
     }
 
   return 0;
@@ -40,14 +42,14 @@ int main(int argc, char *argv[])
   ElemType la[] = {2, 34, 45, 78};
   ElemType lb[] = {2, 32, 34, 77, 78};
 
-  InitList(&Lb);
+  InitList(&Lb); //init La and Lb
   InitList(&La);
   
   for (i = 1; i <= 4; i++)
-    ListInsert(&La, i, la[i - 1]);
+    ListInsert(&La, i, la[i - 1]); // insert element to La
 
   for (i = 1; i <= 5; i++)
-    ListInsert(&Lb, i, lb[i - 1]);
+    ListInsert(&Lb, i, lb[i - 1]); // inset element to Lb
 
   Union(&La, Lb);
 
