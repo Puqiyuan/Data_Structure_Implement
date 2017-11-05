54462692a7b837fc15fb3569c6b24fc084140c7c
Qiyuan Pu <pqy7172@gmail.com>
2017-11-05 15:43:55 +0800

    add header file which used by all c programs

---
+++ triplet_operations_ch1/bigHeader.h
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <ctype.h> // testing and mapping characters
+#include <malloc.h>
+#include <limits.h>
+#include <stdio.h>
+#include <io.h>
+#include <math.h>
+#include <process.h>
+#include <iostream.h>
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+
+typedef int Status;
+typedef int Boolean;
