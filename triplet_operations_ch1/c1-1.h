358be2de7ce85ca7449e39e7c6f6614ce84c19a7
Qiyuan Pu <pqy7172@gmail.com>
2017-11-05 15:53:08 +0800

    add header file that used for chapter1

---
+++ triplet_operations_ch1/c1-1.h
@@ -0,0 +1,2 @@
+// filename: c1-1.h. The storing structre is order and adopting dynamic allocation.
+typedef ElemType *Triplet; // The space of three elements is allocated by InitTriplet function. The type of triplet is a pointer which point into ElemType type, it stores the address of ElemType type.
