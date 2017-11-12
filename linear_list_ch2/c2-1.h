ef37ed74814e47a68b4f3b2437deeb125cdafa2c
Qiyuan Pu <pqy7172@gmail.com>
2017-11-12 23:22:01 +0800

    header file c2-1.h

---
+++ linear_list_ch2/c2-1.h
@@ -0,0 +1,11 @@
+// c2-1.h The dynamic store structre of linear list.
+#define LIST_INIT_SIZE 10 // The initially store space of linear list.
+
+#define LISTINCREMENT 2 // The increment of space.
+
+struct SqList
+{
+	ElemType *elem; // the base address of space.
+	int length; // current length of linear list.
+	int listsize; // current malloced space(in sizeof(ElemType))
+}
