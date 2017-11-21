296a2397f9e83232dc5a65a28fee2a67d12acc7e
Qiyuan Pu <pqy7172@gmail.com>
2017-11-21 20:13:19 +0800

    compete c2-2.h for link_list.

--- linear_list_ch2/link_list/c2-2.h
+++ linear_list_ch2/link_list/c2-2.h
@@ -1,1 +1,8 @@
-// 
+// c2-2.h The store structre of linear list of link form.
+struct LNode
+{
+	ElemType data;
+	LNode *next;
+};
+typedef LNode *LinkList; // The other method to defined LinkList.
+
